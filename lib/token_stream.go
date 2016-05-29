9c82f857b1753bc7eb91ba713b5eca0d7959d5b8
horpto <__singleton__@hackerdom.ru>
2016-05-29 22:09:23 +0500

    add token_stream

---
+++ lib/token_stream.go
@@ -0,0 +1,56 @@
+package boolParser
+
+type TokenStream interface {
+	topToken() Token
+	popToken()
+	putToken(Token)
+}
+
+type ArrayTokenStream struct {
+	eof   bool
+	top   Token // cache
+	stack []*Token
+	ch    <-chan Token
+}
+
+func mkArrayTokenStream(ch <-chan Token) ArrayTokenStream {
+	top, ok := <-ch
+	if !ok {
+		top = Token{_type: tokEOF, offset: 0}
+	}
+	return ArrayTokenStream{
+		eof:   !ok,
+		top:   top,
+		stack: nil, // maybe fail with len()
+		ch:    ch,
+	}
+}
+
+func (ts ArrayTokenStream) topToken() Token {
+	return ts.top
+}
+
+func (ts *ArrayTokenStream) popToken() {
+	if ts.eof {
+		return
+	}
+	if len(ts.stack) > 0 {
+		lastItemIndex := len(ts.stack) - 1
+		ts.top = *ts.stack[lastItemIndex]
+		ts.eof = ts.top._type != tokEOF
+		ts.stack = ts.stack[:lastItemIndex]
+		return
+	}
+
+	top, ok := <-ts.ch
+	if !ok {
+		top = Token{_type: tokEOF, offset: 0}
+	}
+	ts.top = top
+	ts.eof = ts.top._type == tokEOF
+}
+
+func (ts *ArrayTokenStream) putToken(token Token) {
+	ts.stack = append(ts.stack, &ts.top)
+	ts.top = token
+}
